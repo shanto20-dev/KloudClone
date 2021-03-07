59a53c3ac7743504d3c0629bf2216a772d6409ba
shanto20-dev <ecshanto@gmail.com>
2021-03-07 17:19:22 -0500

    Added webpack config and entry file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './frontend/kloud_clone.jsx',
+    output: {
+        filename: 'app/assets/javascripts/bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
