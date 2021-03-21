43edfc879716bf54761c8d5f43ea1418a041cd4f
shanto20-dev <ecshanto@gmail.com>
2021-03-21 13:14:05 -0400

    Changed muting to how soundcloud does it

--- frontend/components/music_player/player.js
+++ frontend/components/music_player/player.js
@@ -6,7 +6,8 @@ class Player extends React.Component{
         super(props)
         this.state = {
             timeCounter: 0,
-            volume: 50,
+            prevVol: 100,
+            volume: 100,
             volumeHover: false
         }
         this.audioEl = React.createRef();
@@ -28,13 +29,15 @@ class Player extends React.Component{
     }
 
     handleMute(){
-        console.log(this.props.muted)
         if (!this.props.songMuted){
+            this.setState({prevVol: this.audioEl.current.volume * 100})
             this.props.muteSong()
-            this.audioEl.current.muted = true;
+            this.audioEl.current.volume = 0;
+            this.setState({volume: 0})
         }else if (this.props.songMuted){
             this.props.muteSong()
-            this.audioEl.current.muted = false;
+            this.audioEl.current.volume = this.state.prevVol / 100;
+            this.setState({ volume: this.state.prevVol })
         }
     }
 
@@ -87,12 +90,18 @@ class Player extends React.Component{
         let duration;
         this.audioEl.current ? duration = Math.floor(this.audioEl.current.duration) : duration = 0;
         this.props.songPlaying ? songActionContent = <i className="fas fa-pause playerpause"></i> : songActionContent = <i className="fas fa-play playerplay"></i>;
-        this.props.songMuted ? muteContent = <i class="fas fa-volume-off playerVol" onClick={this.handleMute}></i> : muteContent = <i class="fas fa-volume-up playerVol" onClick={this.handleMute}></i>;
+        this.props.songMuted ? muteContent = <i className="fas fa-volume-off playerVol" onClick={this.handleMute}></i> : muteContent = <i className="fas fa-volume-up playerVol" onClick={this.handleMute}></i>;
         return(
             <div className = "player" >
                 <audio src={vibeFrogUrl} ref={this.audioEl}></audio>
                 <div className = "controls">
-                    <button onClick={this.handleAction} className="playpause playerButton"> {songActionContent} </button>
+                    <div className="player-left-buttons">
+                        <button className="playerButton"><i className="fas fa-step-backward "></i></button>
+                        <button onClick={this.handleAction} className="playpause playerButton"> {songActionContent} </button>
+                        <button className="playerButton"><i className="fas fa-step-forward"></i></button>
+                        <button className="playerButton"><i className="fas fa-random"></i></button>
+                        <button className="playerButton"><i className="fas fa-redo"></i></button>
+                    </div>
                     <p className="currentTime">{this.formatTime()}</p>
                     <input type='range' className='progslider' value={this.state.timeCounter} ref={this.progressSlider} min="0" max={duration} onInput={this.handleTimeinput}/>
                     <br/>
